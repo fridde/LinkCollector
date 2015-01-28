ebb34e1b7d8435003097db9bfbcde53b1ef8ef03
fridde <f@hehl.se>
2015-01-28 21:59:05 +0100

    Update include.php

--- include.php
+++ include.php
@@ -5,9 +5,9 @@ header('Content-Type: text/html; charset=utf-8');
 $php_inc_files = array("inc/idiorm", "inc/functions");
 $js_inc_files = array(
 	"//ajax.googleapis.com/ajax/libs/jquery/2.1.1/jquery.min.js",
-    "inc/jquery.dataTables.min.js",
+    "//cdn.datatables.net/1.10.4/js/jquery.dataTables.min.js",
 	"//cdn.datatables.net/responsive/1.0.1/js/dataTables.responsive.js",
-	"inc/dataTables.fixedHeader.js", 
+	"//cdn.datatables.net/fixedheader/2.1.2/js/dataTables.fixedHeader.min.js", 
 	"inc/datatables_init.js");
 
 foreach ($php_inc_files as $file) {
